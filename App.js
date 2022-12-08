04c69f81ff8dd8b69c4a830589a257d0d63ebce8
FIRST_NAME LAST_NAME <57823963+Muzammil-crypto@users.noreply.github.com>
2022-12-08 16:34:30 +0500

    Broke Modules

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
 import Login from "./src/screens/Login";
-import Register from "./src/screens/Register";
 
 export default function App() {
   return <Login />;
